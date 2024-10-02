58f4464cd55e18a8f425bb0f6b4db8442fc44050
Jesus Pujada <jpujada30@gmail.com>
2024-10-02 13:25:21 +0200

    Ejercicio 5, en dev

--- UF1/Practica2/JS/script.js
+++ UF1/Practica2/JS/script.js
@@ -29,12 +29,42 @@ function ejercicio3(){
     console.log("El valor: ºC " + input + " de ºC a ºF es -> " + operacionDeCaF);
     
 }
+
 function ejercicio3n(){
     let input = prompt("Introduce el valor que quieras convertir");
     let operacionDeFaC = input-32;
     let operacionDeFaC2 = operacionDeFaC * 5 /9;
     console.log("El valor: ºF " + input + " de ºF a ºC es -> " + operacionDeFaC2);
 }
+
 function ejercicio4(){
+    let precioPorMinuto = 0.05;
+    let precioInicialPorConexion = 0.10;  
+
+    let minutos = prompt("Introduce los minutos totales de la llamada"); // -> 10
+    let segundos = prompt("Introduce los segundos totales de la llamada"); // ->60
+
+    //Aqui coniverto los valores del prompt que por defecto son STRING los convierto a numeros.
+    minutos = parseInt(minutos);
+    segundos = parseInt(segundos);
     
+    //Aqui hago el calculo de segundos a minutos
+    let calculoDeSegundoAMinutos = minutos + (segundos / 60); // 10 + 60/60 = 11
+        
+    //Y aquí calculamos el precio total de la llamada aplicando los precios que nos indica el enunciado.
+    let precioTotalLlamada = (calculoDeSegundoAMinutos * precioPorMinuto) + precioInicialPorConexion; // 11 * 0.05 + 0.10 = 0.65
+    console.log("Duración llamada: " + minutos + " Minutos " + "Precio a pagar: " + precioTotalLlamada + " €");
+
+}
+
+function ejercicio5(){
+    
+    let entrada = prompt("introduce 5 numeros");
+    
+    entrada = parseInt(entrada);
+    
+    let numerosArray = [];
+    entrada.push(numerosArray);
+
+    console.log(numerosArray);
 }
