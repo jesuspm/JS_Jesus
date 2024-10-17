bdce6bf1a81ce323824235a7336a048983963a68
Jesus Pujada <jpujada30@gmail.com>
2024-10-17 19:01:10 +0200

    ACtuali 17/10

--- PENJAT/js/script.js
+++ PENJAT/js/script.js
@@ -4,7 +4,8 @@ const botonComençaPartida = document.getElementById("botonStart");
 const palabraSecretaChange = document.getElementById("palabraSecretaTexto");
 
 //VARIABLES
-let palabraSecreta;
+let palabraIntroduia;
+let palabraSecreta = [];
 let deshabilitarBoton;
 let deshabilitarInput;
 let letrasAbarraBaja;
@@ -12,21 +13,22 @@ let palabraActual = [];
 
 //Funcion para comenzar partida, funciona OK.
 function start(){
-    palabraSecreta = inputObj.value;
+    palabraIntroduia = inputObj.value.toUpperCase();
     
-    if (palabraSecreta) {
+    if (palabraIntroduia) {
         // Comprobar si la palabra contiene algún número
         let contieneNumero = false;
-        for (let i = 0; i < palabraSecreta.length; i++) {
-            if (!isNaN(palabraSecreta[i])) { // Si encuentra un número, establece contieneNumero a true
+        for (let i = 0; i < palabraIntroduia.length; i++) {
+            if (!isNaN(palabraIntroduia[i])) { // Si encuentra un número, establece contieneNumero a true
                 contieneNumero = true;
                 break;
             }
         }
     
         if (!contieneNumero) { // Si no contiene números
-            if (palabraSecreta.length > 3) { // Controlamos si la palabra es mayor que 3.
-                console.log(palabraSecreta.split("")); // Separar la palabra en letras
+            if (palabraIntroduia.length > 3) { // Controlamos si la palabra es mayor que 3.
+                palabraSecreta = palabraIntroduia.split("");
+                console.log(palabraSecreta); // Separar la palabra en letras
 
                 palabraActualInicial();
 
@@ -40,10 +42,11 @@ function start(){
     } else {
         alert("No has introduït cap paraula.");
     }
-    
-    //Deshabilitar boton comença partida y input
-    deshabilitarBoton = botonComençaPartida.disabled = true;
-    deshabilitarInput = inputObj.disabled = true;
+
+    //Llamada a la función que cambia las letras a _ _ _ _
+    cambioDeLetras();
+    //Llamada a la función que deshabilita los botones.
+    deshabilitaboto();
 
 }
 //Funciona ok
@@ -57,6 +60,7 @@ function showPassword(){
     }
 }
 
+//Transorma la palabra y hace un push al array creado en la global.
 function palabraActualInicial(){
     for (let i = 0; i < palabraSecreta.length; i++) {
         palabraActual.push("_");
@@ -64,27 +68,39 @@ function palabraActualInicial(){
     console.log(palabraActual);
 }
 
+//Cambia el array palabraActual que viene por defecto [_,_,_,] a -> _ _ _ gracias al replaceAll(",", "")
 function cambioDeLetras(){
     letrasAbarraBaja = palabraSecretaChange;
     letrasAbarraBaja.textContent = palabraActual.toString().replaceAll(",", "");
 }
 
 
-// function jugarLletra(obj){
-//     let  lletraJugada = obj.textContent;
-//     const aux = palabraSecreta.includes(lletraJugada);
-//     const aux2 = palabraSecreta.indexof(lletraJugada);
 
-//     if (palabraSecreta.includes(lletraJugada)) {
-//         for(let i = 0; i < palabraSecreta.length; i++){
-//             if (palabraSecreta[i] === lletraJugada) {
-//                 palabraActual[i] = lletraJugada;
-//             }
-//         }
-//         cambioDeLetras
-//     }
-// }
+function jugarLletra(obj){ //le pasamos como parametro obj que hace referencia a cada this de las letras.
+    let lletraJugada = obj.textContent; // variable donde almacenamos el contenido de cada boton.
+    
+    // const aux = palabraSecreta.includes(lletraJugada);
+    // console.log(aux);
+    
+    // const aux2 = palabraSecreta.indexof(lletraJugada);
+    // console.log(aux2);
+
+    if (palabraSecreta.includes(lletraJugada)) {
+        for(let i = 0; i < palabraSecreta.length; i++){
+            if (palabraSecreta[i] === lletraJugada) {
+                palabraActual[i] = lletraJugada;
+                //FALTA POR AÑADIR LA LETRA EN EL ARRAY DE LAS _ _ _ _ 
+            }
+        }
+        
+    }else{
+        alert("Letra erronea!") // AÑADIR LO DE LAS IMAGENES POR CADA FALLO.
+    }
+    
+}
+
+//Funcion para deshabilitar los botones una vez comience la partida.
 function deshabilitaboto(){
-    const botoA = document.getElementById("boto_A");
-    botoA.disabled = true;
+    deshabilitarBoton = botonComençaPartida.disabled = true;
+    deshabilitarInput = inputObj.disabled = true;
 }
