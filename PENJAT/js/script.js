c141a7e5ddd8c63816722a0c3bcf54d6135d98c1
Jesus Pujada <jpujada30@gmail.com>
2024-10-27 20:42:31 +0100

    actu

--- PENJAT/js/script.js
+++ PENJAT/js/script.js
@@ -2,17 +2,25 @@
 const inputObj = document.getElementById("palabraOculta");
 const botonComençaPartida = document.getElementById("botonStart");
 const palabraSecretaChange = document.getElementById("palabraSecretaTexto");
+const letraOn = document.getElementById("boton_1");
 
 //VARIABLES
 let palabraIntroduia;
 let palabraSecreta = [];
+
 let deshabilitarBoton;
 let deshabilitarInput;
+let deshabilitarLetra;
 let letrasAbarraBaja;
-let palabraActual = [];
 
+let palabraActual = [];
+let contador = 10;
+
+
+deshabilitarBtn();
 //Funcion para comenzar partida, funciona OK.
 function start(){
+    habilitarBtn();
     palabraIntroduia = inputObj.value.toUpperCase();
     
     if (palabraIntroduia) {
@@ -60,6 +68,8 @@ function showPassword(){
     }
 }
 
+
+
 //Transorma la palabra y hace un push al array creado en la global.
 function palabraActualInicial(){
     for (let i = 0; i < palabraSecreta.length; i++) {
@@ -77,25 +87,70 @@ function cambioDeLetras(){
 
 
 function jugarLletra(obj){ //le pasamos como parametro obj que hace referencia a cada this de las letras.
+    
     let lletraJugada = obj.textContent; // variable donde almacenamos el contenido de cada boton.
     
-    // const aux = palabraSecreta.includes(lletraJugada);
-    // console.log(aux);
-    
-    // const aux2 = palabraSecreta.indexof(lletraJugada);
-    // console.log(aux2);
-
     if (palabraSecreta.includes(lletraJugada)) {
         for(let i = 0; i < palabraSecreta.length; i++){
             if (palabraSecreta[i] === lletraJugada) {
                 palabraActual[i] = lletraJugada;
-                //FALTA POR AÑADIR LA LETRA EN EL ARRAY DE LAS _ _ _ _ 
+                
+                obj.disabled = true;
+                obj.style.borderWidth = "1px";
+                obj.style.borderStyle = "solid";
+                obj.style.borderColor = "red"; // deshabilitas la letra cuando la usas.
+                
+                //AÑADIR LA LETRA EN EL ARRAY DE LAS
+                enviarLetraAlHTML();
             }
         }
         
-    }else{
+    }else if (!palabraSecreta.includes(lletraJugada)) {
         alert("Letra erronea!") // AÑADIR LO DE LAS IMAGENES POR CADA FALLO.
+        obj.disabled = true; //Deshabilita la letra cuando la usas.
+        
+        obj.style.borderWidth = "1px";
+        obj.style.borderStyle = "solid";
+        obj.style.borderColor = "red";
+        //Decrementamos el contador cuando fallamos una letra.
+        contador--;
+    
+        //Filtro para cuando el contador llega a 0 y quiero que ejecute la funcion de verificarDerrota()
+        if (contador == 0) {
+            verificarDerrota();
+        }
+
+        // REVISAR: Lo hace bien, pero si seguimos dandole click a letras que no son sigue decrementando
     }
+
+    //Si acierta todas las letras llamará a la siguiente función.
+    verificarVictoria();
+}
+
+function enviarLetraAlHTML(){
+    //Aqui obtenemos nuevamente el objeto palabraSecretaTexto que seria en el caso de introducir
+    //CASA si pulsamos la A enviaLetra.textContent -> _A_A y con el .join ya que esto añade la letra al array.
+    let enviaLetra = document.getElementById("palabraSecretaTexto");
+    
+    enviaLetra.textContent = palabraActual.join(' ');
+}
+
+// Verificar si el jugador ha ganado (si ya no hay "_")
+function verificarVictoria() {
+    if (palabraActual.join('') === palabraSecreta.join('')) {
+        // Cambiar el color de fondo del h1 al acertar la palabra
+        const h1Elemento = document.getElementById('palabraSecretaTexto');
+        h1Elemento.style.backgroundColor = "lightgreen"; // Cambia el color de fondo del h1
+        alert("¡Has acertado la palabra!");
+    }
+}
+
+//Funcion para pintar el h1 cuando pierde la partida.
+function verificarDerrota(){
+     
+    alert("Se han terminado las oportunidades!")
+    const h1ElementoRojo = document.getElementById('palabraSecretaTexto');
+    h1ElementoRojo.style.backgroundColor = "red";
     
 }
 
@@ -103,4 +158,60 @@ function jugarLletra(obj){ //le pasamos como parametro obj que hace referencia a
 function deshabilitaboto(){
     deshabilitarBoton = botonComençaPartida.disabled = true;
     deshabilitarInput = inputObj.disabled = true;
+}
+
+function estiloLetras(){
+    //Aqui aplicamos los estilos al darle a comenzar partida:
+    obj.style.color = "black";
+    obj.style.borderColor = "black";
+}
+
+
+// //Falta por implementar
+// function imagenesColgado(){
+//     let fotosColgado = document.getElementById('imagenesColgado')
+// }
+
+//Funcion para recorrer los botones de uno en uno mediante la id
+function deshabilitarBtn(){
+    //Creamos una constante con la parte que nunca cambia de la ID.
+    const literslFijo = "boton_";
+
+    //Luego haremos un bucle for que irá desde el 1 hasta el 27(que son la cantidad de boton_ 
+    //que hay como id en el html y lo sumaremos de 1 en 1)
+    for(let numObj = 1; numObj < 28; numObj++){
+        //Concatenamos el "boton_" + 1 , "boton_"+2 etc..
+        let literalObj = literslFijo + numObj;
+        let button = document.getElementById(literalObj);
+        button.disabled = true;
+ 
+        //Estilos.        
+        button.style.backgroundColor = "greenyellow";
+        button.style.borderWidth = "1px";
+        button.style.borderStyle = "solid";
+        button.style.borderColor = "red";
+
+    }
+}
+function habilitarBtn(){
+    //Creamos una constante con la parte que nunca cambia de la ID.
+    const literslFijo = "boton_";
+
+    //Luego haremos un bucle for que irá desde el 1 hasta el 27(que son la cantidad de boton_ 
+    //que hay como id en el html y lo sumaremos de 1 en 1)
+    for(let numObj = 1; numObj < 28; numObj++){
+        //Concatenamos el "boton_" + 1 , "boton_"+2 etc..
+        let literalObj = literslFijo + numObj;
+
+        let button = document.getElementById(literalObj);
+        
+        //Estilos
+        button.style.backgroundColor = "greenyellow";
+        button.style.borderWidth = "1px";
+        button.style.borderStyle = "solid";
+        button.style.borderColor = "black"
+
+        //Lo deshabilitamos 1 a 1.
+        button.disabled = false;
+    }
 }
